f2def4d525f6c124759eaaf5397eef84121b1bd8
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-12-05 09:12:07 -0300

    criação botão de busca

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -3,29 +3,57 @@ import 'package:provider/provider.dart';
 import '../models/appdata.dart';
 import '../partials/customAppBar.dart';
 import '../partials/customDrawer.dart';
+import '../partials/cityBox.dart';
 
 class SearchPage extends StatelessWidget {
+  var listaResultadoBusca = [];
+
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Consumer<AppData>(
-        builder: (ctx, appdata, child) => Scaffold(
-              key: _scaffoldKey,
-              appBar: CustomAppBar(
-                scaffoldKey: _scaffoldKey,
-                pageContext: context,
-                title: 'Busque seu destino',
-                hideSearch: true,
+      builder: (ctx, appdata, child) => Scaffold(
+        key: _scaffoldKey,
+        appBar: CustomAppBar(
+          scaffoldKey: _scaffoldKey,
+          pageContext: context,
+          title: 'Busque seu destino',
+          hideSearch: true,
+        ),
+        drawer: CustomDrawer(pageContext: context),
+        backgroundColor: Colors.white,
+        body: Column(children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Digite o nome da cidade',
+                border: OutlineInputBorder(),
+                suffixIcon: Icon(
+                  Icons.search,
+                  size: 30,
+                ),
               ),
-              drawer: CustomDrawer(pageContext: context),
-              backgroundColor: Colors.white,
-              body: Center(
-                  heightFactor: 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[Text('Página de busca')],
-                  )),
-            ));
+            ),
+          ),
+          GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              children: List.generate(
+                listaResultadoBusca.length,
+                ((index) {
+                  return CityBox(
+                    data: listaResultadoBusca[index],
+                    onTap: (cityData) {
+                      // ignore: avoid_print
+                      print(cityData['name']);
+                    },
+                  );
+                }),
+              )),
+        ]),
+      ),
+    );
   }
 }
